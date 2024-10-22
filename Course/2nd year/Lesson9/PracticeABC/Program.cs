06540f89566e3d7ac22d338a435e1dc86ea077af
RMP1Games <tynitskyivlad@gmail.com>
2024-10-22 22:33:12 +0300

    Do practiceABC in Lesson9

--- Course/2nd year/Lesson9/PracticeABC/Program.cs
+++ Course/2nd year/Lesson9/PracticeABC/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Loader;
 
 namespace SimpleBD
 {     
@@ -65,11 +66,58 @@ namespace SimpleBD
         }
     }
 
+        class Person
+    {
+        public string name;
+        public int age;
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, my name is " + name);
+        }
+        // public void AgeChecker(int Age)
+        // {
+        //     if (Age < 0)
+        //     Console.WriteLine("Your age can't be smaller than 0.");
+        //     else
+        //     age = Age;
+        // }
+        public Person (string Name, int Age)
+        {
+            name = Name;
+            if (Age < 0)
+            Console.WriteLine("Age < 0. Write real age.");
+            else
+            age = Age;
+        }
+    }
+
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello world!")
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("=====================");
+
+            Person person1 = new Person("Alex", 16);
+            person1.Introduce();
+
+            Console.WriteLine("=====================");
+
+            Person name1 = new Person("Sasha", 16);
+            Person name2 = new Person("Max", 15);
+            Person name3 = new Person("Anna", 16);
+            Person name4 = new Person("Artem", 14);
+            Person[] names = {name1, name2, name3, name4};
+
+            foreach (Person name in names)
+            {
+                name.Introduce();
+            }
+            
+            Console.WriteLine("=====================");
+
+            Person person2 = new Person("Misha", -2);
         }
     }
 }
