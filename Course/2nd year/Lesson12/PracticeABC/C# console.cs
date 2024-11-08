2a0fa4d8c12bd3c39b24372907a26eed241b5494
RMP1Games <tynitskyivlad@gmail.com>
2024-11-08 21:11:14 +0300

    12 Lesson Practice ABC

---
+++ Course/2nd year/Lesson12/PracticeABC/C# console.cs
@@ -0,0 +1,58 @@
+using System;
+public class Program
+{
+    class Product
+    {
+        public string name;
+        public int price;
+        public string description;
+    }
+    class CategoryAndProducts
+    {
+
+    }
+
+    class Order
+    {
+        public int id;
+        public string[] items;
+        public int total; 
+    }
+
+    class User
+    {
+        public string name;
+        public string email;
+        public int purchases;
+    }
+    
+    class Cart
+    {
+
+    }
+
+    class Shipping
+    {
+
+    }
+
+    class Payment
+    {
+
+    }
+
+    class Reviews
+    {
+
+    }
+
+    class Discounts
+    {
+
+    }
+    
+    class Addresses
+    {
+
+    }
+}
