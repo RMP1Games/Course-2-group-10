5b7fa8cf2f56bc15e3e5f07072de85c65e2781d9
RMP1Games <tynitskyivlad@gmail.com>
2025-04-01 20:03:37 +0300

    Practice from 18_03_25

---
+++ vs files/180325-/180325-/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180325_.Models
+{
+    internal class Book
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+        public string genre { get; set; }
+    }
+}
---
+++ vs files/210325async/210325async/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Net.WebRequestMethods;
+using System.Threading.Tasks;
+
+namespace _210325async
+{
+    public class _210325async
+    {
+        public static async Task<string> FetchDataAsync(string url)
+        {
+            try
+            {
+                var client = new HttpClient();
+                return await client.GetStringAsync(url);
+            }
+            catch (Exception ex)
+            {
+                return $"Ошибка при загрузке данных c использованием ({url}): {ex.Message}";
+            }
+        }
+
+        public static async Task Main()
+        {
+            List<string> urls = new List<string>
+        {"https://jsonplaceholder.typicode.com/posts/1","https://dog.ceo/api/breeds/image/random","https://randomuser.me/api/", "https://invalid-api.example"};
+            var tasks = urls.Select(url => FetchDataAsync(url)).ToList();
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+                Console.WriteLine(" ");
+                Console.WriteLine("--------------------");
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
