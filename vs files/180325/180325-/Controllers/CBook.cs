86a3c27180ba1b458d7a92d8d1ff82c50ae0fdde
RMP1Games <tynitskyivlad@gmail.com>
2025-04-01 20:07:17 +0300

    Practice 18_03_25

---
+++ vs files/180325/180325-/Controllers/CBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _180325_.Models;
+
+namespace _180325_.Controllers
+{
+    internal class CBook
+    {
+        public static List<Book> Books = new List<Book>();
+        public void CreateBook(string Name, string Author, string Genre)
+        {
+            Book newbook = new Book();
+            newbook.name = Name;
+            newbook.author = Author;
+            newbook.genre = Genre;
+            Books.Add(newbook);
+        }
+
+        public void UpdateBook(Book bookToChange, string Name, string Author, string Genre)
+        {
+            bookToChange.name = Name;
+            bookToChange.author = Author;
+            bookToChange.genre = Genre;
+        }
+
+        public void DeleteBook(Book book)
+        {
+            book = null;
+        }
+
+        public List<Book> GetBooks()
+        {
+            return Books;
+        }
+    }
+}
---
+++ vs files/180325/180325-/Services/Interfaces/ICBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _180325_.Models;
+
+namespace _180325_.Services.Interfaces
+{
+    internal interface ICBook
+    {
+        void CreateUser(string Name, string Author, string Genre);
+        void UpdateUser(Book bookToChange, string Name, string Author, string Genre);
+        void DeleteUser(Book book);
+        List<Book> GetBooks();   
+    }
+}
